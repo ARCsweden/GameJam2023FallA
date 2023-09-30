addae5d1a6823ef5d56c71ce3c6dd60c36a26af1
Moritz Schmidt <moritz_schmidt_1@web.de>
2023-09-30 20:03:26 +0200

    Added grenade physics

--- Assets/Player/PlayerScript.cs
+++ Assets/Player/PlayerScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public GameObject grenade;
+    Rigidbody grenade_Rigidbody;
+    public float impulsez = 2;
+    public float impulsey = 4;
+    //public Vector3 offset = new Vector3(1.0f,0.0f,0.0f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +27,16 @@ public class PlayerScript : MonoBehaviour
         gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0,5,0), ForceMode.Impulse);
     }
 
+    void OnThrow()
+    {
 
+        GameObject g = Instantiate(grenade, transform.position+transform.forward , transform.rotation) ;
+        g.GetComponent<Rigidbody>().AddForce(0, impulsey, impulsez, ForceMode.Impulse);
+        UnityEngine.Debug.Log("NEW INPUT SYSTEM, g key pressed");
+        Destroy(g, 5.0f);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.name);
+    }
 }
